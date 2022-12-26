568874fdf2d8842ecb7c0ad48a2a56313267d99a
yuikoito <yuiko.dev@gmail.com>
2022-12-26 09:16:53 +0900

    delete i18n

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  i18n: {
-    locales: ["en", "ja"],
-    defaultLocale: "en",
-  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
