03e2582ecac64d266243cda5d89e7c3b60066ef0
yuikoito <yuiko.dev@gmail.com>
2022-12-31 23:37:44 +0900

    fix: fix color

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -2,7 +2,7 @@ import { Center } from "@chakra-ui/react";
 import styles from "../styles/Loader.module.css";
 const Loader = () => {
   return (
-    <Center bgColor='gray.200' h='100vh'>
+    <Center bgColor="orange.100" h="100vh">
       <div className={styles.loader5}></div>
     </Center>
   );
